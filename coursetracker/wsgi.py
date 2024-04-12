fb2722e57324878869447f8ac9976ca0327f8173
Ashley Sands <ashley.sands01@gmail.com>
2024-04-12 16:21:59 -0400

    course tracker rebrand

--- coursetracker/wsgi.py
+++ coursetracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for coursecollector project.
+WSGI config for coursetracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coursecollector.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coursetracker.settings')
 
 application = get_wsgi_application()
